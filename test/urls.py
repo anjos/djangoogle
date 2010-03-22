31eb4fbca9a10af48837567094edec625500e741
André Anjos <rabello@pcuw32.cern.ch>
2010-03-22 14:38:38 +0100

    Updated build and test infrastructure

--- test/urls.py
+++ test/urls.py
@@ -1,26 +1,26 @@
+import os 
 import settings
 from django.conf.urls.defaults import *
 from django.contrib import admin
-import djangoogle.urls
+from django.views.generic.simple import redirect_to, direct_to_template
+
+exec 'from %s.urls import namespaced as test_urls' % settings.PROJECT
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    (r'^$', 'django.views.generic.simple.direct_to_template', {'template': 'home.html'}), 
-    (r'^admin/(.*)', admin.site.root),
-    (r'^google/', djangoogle.urls.namespaced),
-    (r'^i18n/', include('django.conf.urls.i18n')),
-    (r'^jsi18n/(?P<packages>\S+?)/$', 'django.views.i18n.javascript_catalog'),
-    (r'^jsi18n/$', 'django.views.i18n.javascript_catalog'),
-    (r'^rosetta/', include('rosetta.urls')),
+    url(r'^$', direct_to_template, {'template': 'home.html'}),
+    url(r'^p/', test_urls),
+    url(r'^admin/', admin.site.urls),
+    url(r'^i18n/', include('django.conf.urls.i18n')),
+    url(r'^jsi18n/(?P<packages>\S+?)/$', 
+      'django.views.i18n.javascript_catalog'),
+    url(r'^jsi18n/$', 'django.views.i18n.javascript_catalog'),
+    url(r'^rosetta/', include('rosetta.urls')),
 
     # Media serving
-    url(r'^media/(?P<path>.*)$', 
-        'django.views.static.serve',
-        {
-         'document_root': settings.MEDIA_ROOT,
-         'show_indexes': True,
-        }, 
-        name='media',
-        ), 
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+     {'document_root': settings.MEDIA_ROOT,
+     'show_indexes': True},
+     name='media'), 
     )
