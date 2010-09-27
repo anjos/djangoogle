9ecc1ebd7e37eca5f1425282b7fae8eb0c5743c0
André Anjos <andre.anjos@idiap.ch>
2010-09-27 16:34:23 +0200

    Change field size to accomodate MySQL limitations

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup, find_packages
 setup(
 
     name = "djangoogle",
-    version = "0.5.3", 
+    version = "0.5.4", 
     packages = find_packages(),
 
     # we also need all translation files and templates
